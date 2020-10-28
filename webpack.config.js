06a4190c939554b2275ecc48639c0dfcd6be015e
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-10-28 09:35:26 +0900

    babel uninstall

--- webpack.config.js
+++ webpack.config.js
@@ -8,19 +8,8 @@ module.exports = {
     contentBase: "dist",
     open: true
   },
-
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
       {
         test: /\.ts$/,
         use: 'ts-loader',
