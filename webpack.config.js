64187d4fbee1f2d38a274e9165465078410530ea
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-11-02 09:18:49 +0900

    delete not use import

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { PollingWatchKind, isJSDocPublicTag } = require('typescript');
 const MODE = "development";
 const enabledSourceMap = MODE === "development";
 
