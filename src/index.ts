d7a90a069cb812a667ba1c9a9fc309482dc78752
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-11-26 10:47:43 +0900

    JSで部品をロード

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 import "./scss/style.scss";
+
+const navComponent = require('./common/nav.html');
+const topComponent = require('./common/top.html')
+
+const nav = document.getElementById('nav');
+nav.innerHTML = navComponent;
+const top = document.getElementById('top');
+top.innerHTML = topComponent;
